b87599477aa81901745304e5233aace30f325918
Isabella <isabellaj.lizarde@gmail.com>
2023-11-15 01:45:16 -0800

    Created new components for project

--- src/App.js
+++ src/App.js
@@ -1,41 +1,11 @@
-import { useState } from "react";
 import "./App.css";
-import TaskList from "./TaskList";
+import TodoList from "./TodoList";
 
 function App() {
-  let [addTask, setAddTask] = useState("");
-  // let [taskList, setTaskList] = useState("");
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    // setTaskList();
-    // console.log(addTask);
-    setAddTask(event.target.value);
-  }
-
-  function showTaskList(event) {
-    // event.preventDefault();
-    setAddTask(event.target.value);
-  }
-
   return (
     <div className="App">
-      <header className="App-header">
-        <h1> What's the plan for today? </h1>
-        <form onClick={handleSubmit}>
-          <input
-            type="text"
-            placeholder="Add a task"
-            onChange={showTaskList}
-          ></input>
-          <input
-            type="submit"
-            className="button"
-            value="Add"
-            // onClick={showTaskList}
-          ></input>
-        </form>
-        <TaskList task={addTask} />
+      <header className="todo-app">
+        <TodoList />
       </header>
     </div>
   );
