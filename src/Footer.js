a7a1ba283a43a2234545beff10d8478c502403a7
Isabella <isabellaj.lizarde@gmail.com>
2023-11-24 01:58:52 -0800

    Updated Footer component

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,17 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="Footer">
-      <footer className="footer">Made with ♥︎ by Isabella Lizarde 2023</footer>
+      <footer className="footer">
+        Made with ♥︎ by{" "}
+        <a
+          href="https://github.com/soybella/todo-list-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Isabella Lizarde{" "}
+        </a>{" "}
+        2023
+      </footer>
     </div>
   );
 }
