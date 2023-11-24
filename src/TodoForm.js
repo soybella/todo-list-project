f576522481399b4bab04c5c1b4da1334f5be3055
Isabella <isabellaj.lizarde@gmail.com>
2023-11-24 13:34:15 -0800

    Created capitalizeFirst letter function
    Input will automatically have a capitalization for each todo

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -12,13 +12,17 @@ export default function TodoForm({ onSubmit }) {
     setNewTodoItem("");
   }
 
+  const capitalizeFirst = (e) => {
+    return e.charAt(0).toUpperCase() + e.slice(1);
+  };
+
   return (
     <div className="TodoForm">
       <form onSubmit={handleSubmit} className="todo-form">
         <div className="form-row">
           <input
             value={newTodoItem}
-            onChange={(e) => setNewTodoItem(e.target.value)}
+            onChange={(e) => setNewTodoItem(capitalizeFirst(e.target.value))}
             type="text"
             id="todo-item"
             placeholder="Add a todo..."
